a9e2a084832863e22deb256ceb0cca71c58f2fe6
boobaGreen <claudiodallara77@gmail.com>
2025-02-16 03:45:43 +0100

    no twitter temp

--- fe_cyberpunk/src/utils/getContractAddress.ts
+++ fe_cyberpunk/src/utils/getContractAddress.ts
@@ -7,12 +7,14 @@ export const getContractAddress = async (networkChainId: number, contractName: s
         try {
             const deployedAddresses: { [key: string]: string } = loadDeployedAddresses(networkChainId);
             contractAddress = deployedAddresses[contractName];
+            console.log("contractAddress  in local", contractAddress)
         } catch (error) {
             console.warn('Failed to load deployed addresses, falling back to environment variable');
             contractAddress = import.meta.env.VITE_CONTRACT_ADDRESS;
         }
     } else {
         contractAddress = import.meta.env.VITE_CONTRACT_ADDRESS;
+        console.log("contract address in sepolia , ", contractAddress)
     }
 
     if (!contractAddress) {
