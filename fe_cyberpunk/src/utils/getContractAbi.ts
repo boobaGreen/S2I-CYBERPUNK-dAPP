68e1c9eca372776b35be76301521f12a41dc67da
boobaGreen <claudiodallara77@gmail.com>
2025-02-16 02:19:00 +0100

    no twitter temp

--- fe_cyberpunk/src/utils/getContractAbi.ts
+++ fe_cyberpunk/src/utils/getContractAbi.ts
@@ -1,10 +1,10 @@
 export const getContractABI = async () => {
   if (import.meta.env.MODE === 'production') {
-    return await import('./abis/CyberPunkBoutique.json');
+    return require('./abis/CyberPunkBoutique.json');
   }
   if (import.meta.env.MODE === 'development') {
-    return await import('../../../be_cyberpunk/artifacts/contracts/CyberPunk.sol/CyberPunkBoutique.json');
+    return require('../../../be_cyberpunk/artifacts/contracts/CyberPunk.sol/CyberPunkBoutique.json');
   } else {
-    return await import('./abis/CyberPunkBoutique.json');
+    return require('./abis/CyberPunkBoutique.json');
   }
 };
