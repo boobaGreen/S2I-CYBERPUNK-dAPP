53cf3519b2f5fabad8a14269eb4c98a8fadfc02e
boobaGreen <claudiodallara77@gmail.com>
2025-02-16 03:00:41 +0100

    no twitter temp

--- fe_cyberpunk/vite.config.ts
+++ fe_cyberpunk/vite.config.ts
@@ -4,16 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  build: {
-    rollupOptions: {
-      onwarn(warning, warn) {
-        // Ignore specific warnings related to comments
-        if (warning.message.includes('contains an annotation that Rollup cannot interpret')) {
-          return;
-        }
-        warn(warning);
-      }
-    }
-  }
+  plugins: [react(), tailwindcss(),],
 })
