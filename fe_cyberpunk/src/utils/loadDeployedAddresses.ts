324284dc628a8d3c74983436c98d9b6d4f94d084
boobaGreen <claudiodallara77@gmail.com>
2025-02-16 04:20:38 +0100

    no twitter temp

--- fe_cyberpunk/src/utils/loadDeployedAddresses.ts
+++ fe_cyberpunk/src/utils/loadDeployedAddresses.ts
@@ -1,5 +1,13 @@
-import deployedAddressesLocal from '../../../be_cyberpunk/ignition/deployments/chain-1337/deployed_addresses.json';
-import deployedAddressesSepolia from '../../../be_cyberpunk/ignition/deployments/chain-11155111/deployed_addresses.json';
+// import deployedAddressesLocal from '../../../be_cyberpunk/ignition/deployments/chain-1337/deployed_addresses.json';
+// import deployedAddressesSepolia from '../../../be_cyberpunk/ignition/deployments/chain-11155111/deployed_addresses.json';
+
+const deployedAddressesLocal = {
+    "CyberPunkModule#CyberPunkBoutique": "cb27d7709ddb02f3742496ba6218dea"
+};
+
+const deployedAddressesSepolia = {
+    "CyberPunkModule#CyberPunkBoutique": "cb27d7709ddb02f3742496ba6218deas"
+};
 
 const loadDeployedAddresses = (chainId: number) => {
     switch (chainId) {
