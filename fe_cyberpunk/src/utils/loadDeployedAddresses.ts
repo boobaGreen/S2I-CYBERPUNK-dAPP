5766f04703bec0480fd59f7ca160fe712113eb3d
boobaGreen <claudiodallara77@gmail.com>
2025-02-16 04:53:10 +0100

    no twitter temp

--- fe_cyberpunk/src/utils/loadDeployedAddresses.ts
+++ fe_cyberpunk/src/utils/loadDeployedAddresses.ts
@@ -2,11 +2,11 @@
 // import deployedAddressesSepolia from '../../../be_cyberpunk/ignition/deployments/chain-11155111/deployed_addresses.json';
 
 const deployedAddressesLocal = {
-    "CyberPunkModule#CyberPunkBoutique": "cb27d7709ddb02f3742496ba6218dea"
+    "CyberPunkModule#CyberPunkBoutique": "0xea1A14F7f4938b955413d3d274c5720794419Dea"
 };
 
 const deployedAddressesSepolia = {
-    "CyberPunkModule#CyberPunkBoutique": "cb27d7709ddb02f3742496ba6218dea"
+    "CyberPunkModule#CyberPunkBoutique": "0xea1A14F7f4938b955413d3d274c5720794419Dea"
 };
 
 const loadDeployedAddresses = (chainId: number) => {
