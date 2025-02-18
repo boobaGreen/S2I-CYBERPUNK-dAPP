1b708985b400a2af1685790ef490110ca82a34c2
boobaGreen <claudiodallara77@gmail.com>
2025-02-18 01:54:16 +0100

    developer badgeEnv

--- fe_cyberpunk/src/utils/getContractAddress.ts
+++ fe_cyberpunk/src/utils/getContractAddress.ts
@@ -1,32 +1,24 @@
 import loadDeployedAddresses from './loadDeployedAddresses';
 
-export const getContractAddress = async (networkChainId: number, contractName: string) => {
-    let contractAddress: string | undefined;
-
-    if (import.meta.env.MODE === 'development') {
-        console.log("chainId:", networkChainId);
-        try {
-            // Se non siamo connessi (networkChainId non valido o 0), assumiamo il default di Sepolia (11155111)
-            if (!networkChainId || networkChainId === 0) {
-                networkChainId = 11155111;
-            }
-            const deployedAddresses: { [key: string]: string } = loadDeployedAddresses(networkChainId);
-            contractAddress = deployedAddresses[contractName];
-
-        } catch (error) {
-            console.warn('Failed to load deployed addresses, falling back to environment variable');
-            contractAddress = import.meta.env.VITE_CONTRACT_ADDRESS;
-        }
-    } else {
-        // In produzione uso la variabile d'ambiente
-        contractAddress = import.meta.env.VITE_CONTRACT_ADDRESS;
-
+export const getContractAddress = async (networkChainId: number, contractName: string): Promise<string> => {
+    // Se non siamo connessi o il chainId non è valido, usa il default di Sepolia (11155111)
+    if (!networkChainId || networkChainId === 0) {
+        networkChainId = 11155111;
     }
 
+    let contractAddress: string | undefined;
+
+    try {
+        const deployedAddresses: { [key: string]: string } = loadDeployedAddresses(networkChainId);
+        contractAddress = deployedAddresses[contractName];
+    } catch (error) {
+        console.warn('Failed to load deployed addresses, falling back to environment variable');
+        contractAddress = import.meta.env.VITE_CONTRACT_ADDRESS;
+    }
+    console.log('contractAddress- da getContractAddress.ts', contractAddress);
     if (!contractAddress) {
         throw new Error('Contract address not found');
     }
-
 
     return contractAddress;
 };
